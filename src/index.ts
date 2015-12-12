21f76f0f8fa8bd65e94827f571ca67588adfaa55
S. Chris Colbert <sccolbert@gmail.com>
2015-12-12 15:20:28 -0500

    wrap registry in a container object and only use factories

--- src/index.ts
+++ src/index.ts
@@ -55,35 +55,7 @@ class Token<T> {
 
 
 /**
- * A class which declares its dependencies.
- */
-export
-interface IInjectable<T> {
-  /**
-   * The lifetime policy for the registration.
-   *
-   * The default value is `Lifetime.Singleton`.
-   */
-  lifetime?: Lifetime;
-
-  /**
-   * The dependencies required to instantiate the class.
-   */
-  requires: Token<any>[];
-
-  /**
-   * Create a new instance of the type.
-   *
-   * @param args - The resolved dependencies specified by `requires`.
-   *
-   * @returns A new instance of the type.
-   */
-  new(...args: any[]): T;
-}
-
-
-/**
- * A factory object which declares its dependencies.
+ * A factory which declares its dependencies.
  */
 export
 interface IFactory<T> {
@@ -111,106 +83,120 @@ interface IFactory<T> {
 
 
 /**
- * A type alias for a provider object.
+ * A lightweight dependency injection container.
  */
 export
-type Provider<T> = IInjectable<T> | IFactory<T>;
-
-
-/**
- * Test whether a token is registered.
- *
- * @param token - The run-time type token of interest.
- *
- * @returns `true` if the token is registered, `false` otherwise.
- */
-export
-function isRegistered<T>(token: Token<T>): boolean {
-  return registry.has(token);
-}
-
-
-/**
- * Register a type mapping for the specified token.
- *
- * @param token - The run-time type token of interest.
- *
- * @param provider - The object which will create the instance.
- *
- * #### Notes
- * If the token is already registered, or if registering the provider
- * would cause a circular dependency, an error will be logged to the
- * console and the registration will be ignored.
- */
-export
-function register<T>(token: Token<T>, provider: Provider<T>): void {
-  if (checkRegistered(token)) return;
-  if (checkCyclic(token, provider)) return;
-  registry.set(token, createResolver(provider));
-}
-
-
-/**
- * Resolve an instance for the given token or provider.
- *
- * @param value - The token or provider object to resolve.
- *
- * @returns A promise which resolves to an instance of the requested
- *   type, or rejects if the instance fails to resolve for any reason.
- */
-export
-function resolve<T>(value: Token<T> | Provider<T>): Promise<T> {
-  let result: T | Promise<T>;
-  if (value instanceof Token) {
-    result = resolveToken(value as Token<T>);
-  } else {
-    result = resolveProvider(value as Provider<T>);
+class Container {
+  /**
+   * Test whether a token is registered with the container.
+   *
+   * @param token - The run-time type token of interest.
+   *
+   * @returns `true` if the token is registered, `false` otherwise.
+   */
+  isRegistered(token: Token<any>): boolean {
+    return this._registry.has(token);
   }
-  return Promise.resolve(result);
+
+  /**
+   * Register a type mapping for the specified token.
+   *
+   * @param token - The run-time type token of interest.
+   *
+   * @param factory - The factory which will create the instance.
+   *
+   * #### Notes
+   * If the token is already registered, or if registering the factory
+   * would cause a circular dependency, an error will be logged to the
+   * console and the registration will be ignored.
+   */
+  register<T>(token: Token<T>, factory: IFactory<T>): void {
+    if (this._registry.has(token)) {
+      logRegisterError(token);
+      return;
+    }
+    let cycle = findCycle(this._registry, token, factory);
+    if (cycle.length > 0) {
+      logCycleError(token, cycle);
+      return;
+    }
+    this._registry.set(token, createResolver(factory));
+  }
+
+  /**
+   * Resolve an instance for the given token or factory.
+   *
+   * @param value - The token or factory object to resolve.
+   *
+   * @returns A promise which resolves to an instance of the requested
+   *   type, or rejects with an error if an instance fails to resolve.
+   */
+  resolve<T>(value: Token<T> | IFactory<T>): Promise<T> {
+    let result: T | Promise<T>;
+    if (value instanceof Token) {
+      result = resolveToken(this._registry, value as Token<T>);
+    } else {
+      result = resolveFactory(this._registry, value as IFactory<T>);
+    }
+    return Promise.resolve(result);
+  }
+
+  private _registry = createRegistry();
 }
 
 
 /**
- * An object which manages the resolution of a provider.
+ * An object which manages the resolution of a factory.
  */
 interface IResolver<T> {
   /**
-   * The provider managed by the resolver.
+   * The factory managed by the resolver.
    */
-  provider: Provider<T>;
+  factory: IFactory<T>;
 
   /**
-   * Resolve an instance of the type from the provider.
+   * Resolve an instance of the type from the factory.
    */
-  resolve(): T | Promise<T>;
+  resolve(registry: Registry): T | Promise<T>;
 }
 
 
 /**
- * The internal registry mapping tokens to resolvers.
+ * A type alias for a resolver registry map.
  */
-var registry = new Map<Token<any>, IResolver<any>>();
+type Registry = Map<Token<any>, IResolver<any>>;
 
 
 /**
- * Check and log an error if the token is registered.
+ * Create a new registry instance.
  */
-function checkRegistered<T>(token: Token<T>): boolean {
-  if (!registry.has(token)) return false;
+function createRegistry(): Registry {
+  return new Map<Token<any>, IResolver<any>>();
+}
+
+
+/**
+ * Log an error which indicates a token is already registered.
+ */
+function logRegisterError(token: Token<any>): void {
   console.error(`Token '${token.name}' is already registered.`);
-  return true;
 }
 
 
 /**
- * Check and log an error if the provider causes a cycle.
+ * Log an error which indicates a cycle was detected.
  */
-function checkCyclic<T>(token: Token<T>, provider: Provider<T>): boolean {
-  let cycle = findCycle(token, provider);
-  if (cycle.length === 0) return false;
+function logCycleError(token: Token<any>, cycle: Token<any>[]): void {
   let path = cycle.map(token => `'${token.name}'`).join(' -> ');
   console.error(`Cycle detected: '${token.name}' -> ${path}.`);
-  return true;
+}
+
+
+/**
+ * Create a rejected promise which indicates the token is unregistered.
+ */
+function rejectUnregistered(token: Token<any>): Promise<any> {
+  return Promise.reject(new Error(`Unregistered token: '${token.name}'.`));
 }
 
 
@@ -221,19 +207,19 @@ function checkCyclic<T>(token: Token<T>, provider: Provider<T>): boolean {
  * The given token is the implicit start of the cycle. If no cycle is
  * present, the array will be empty.
  */
-function findCycle<T>(token: Token<T>, provider: Provider<T>): Token<any>[] {
+function findCycle<T>(registry: Registry, token: Token<T>, factory: IFactory<T>): Token<any>[] {
   let trace: Token<any>[] = [];
-  visit(provider);
+  visit(factory);
   return trace;
 
-  function visit(value: Provider<any>): boolean {
-    for (let other of value.requires) {
+  function visit(factory: IFactory<any>): boolean {
+    for (let other of factory.requires) {
       trace.push(other);
       if (other === token) {
         return true;
       }
       let resolver = registry.get(other);
-      if (resolver && visit(resolver.provider)) {
+      if (resolver && visit(resolver.factory)) {
         return true;
       }
       trace.pop();
@@ -244,59 +230,25 @@ function findCycle<T>(token: Token<T>, provider: Provider<T>): Token<any>[] {
 
 
 /**
- * Create a rejected promise which indicates the token is unregistered.
+ * Resolve a token using the specified registry.
  */
-function rejectToken<T>(token: Token<T>): Promise<T> {
-  return Promise.reject(new Error(`Unregistered token: '${token.name}'.`));
-}
-
-
-/**
- * Resolve the given token.
- */
-function resolveToken<T>(token: Token<T>): T | Promise<T> {
+function resolveToken<T>(registry: Registry, token: Token<T>): T | Promise<T> {
   let result: T | Promise<T>;
-  let resolver = registry.get(token);
+  let resolver = registry.get(token) as IResolver<T>;
   if (resolver) {
-    result = resolver.resolve();
+    result = resolver.resolve(registry);
   } else {
-    result = rejectToken(token);
+    result = rejectUnregistered(token);
   }
   return result;
 }
 
 
 /**
- * Resolve the given provider.
+ * Resolve a factory using the specified registry.
  */
-function resolveProvider<T>(provider: Provider<T>): Promise<T> {
-  let result: Promise<T>;
-  if (typeof provider === 'function') {
-    result = resolveInjectable(provider as IInjectable<T>);
-  } else {
-    result = resolveFactory(provider as IFactory<T>);
-  }
-  return result;
-}
-
-
-/**
- * Resolve the given injectable.
- */
-function resolveInjectable<T>(injectable: IInjectable<T>): Promise<T> {
-  let promises = injectable.requires.map(resolveToken);
-  return Promise.all(promises).then(dependencies => {
-    let instance = Object.create(injectable.prototype);
-    return injectable.apply(instance, dependencies) || instance;
-  });
-}
-
-
-/**
- * Resolve the given factory.
- */
-function resolveFactory<T>(factory: IFactory<T>): Promise<T> {
-  let promises = factory.requires.map(resolveToken);
+function resolveFactory<T>(registry: Registry, factory: IFactory<T>): Promise<T> {
+  let promises = factory.requires.map(token => resolveToken(registry, token));
   return Promise.all(promises).then(dependencies => {
     return factory.create.apply(factory, dependencies);
   });
@@ -304,14 +256,14 @@ function resolveFactory<T>(factory: IFactory<T>): Promise<T> {
 
 
 /**
- * Create a resolver for the given provider and options.
+ * Create a resolver for the given factory.
  */
-function createResolver<T>(provider: Provider<T>): IResolver<T> {
+function createResolver<T>(factory: IFactory<T>): IResolver<T> {
   let result: IResolver<T>;
-  if (provider.lifetime === Lifetime.Transient) {
-    result = new TransientResolver(provider);
+  if (factory.lifetime === Lifetime.Transient) {
+    result = new TransientResolver(factory);
   } else {
-    result = new SingletonResolver(provider);
+    result = new SingletonResolver(factory);
   }
   return result;
 }
@@ -324,25 +276,25 @@ class TransientResolver<T> implements IResolver<T> {
   /**
    * Construct a new transient resolver.
    */
-  constructor(provider: Provider<T>) {
-    this._provider = provider;
+  constructor(factory: IFactory<T>) {
+    this._factory = factory;
   }
 
   /**
-   * The provider managed by the resolver.
+   * The factory managed by the resolver.
    */
-  get provider(): Provider<T> {
-    return this._provider;
+  get factory(): IFactory<T> {
+    return this._factory;
   }
 
   /**
-   * Resolve an instance of the type from the provider.
+   * Resolve an instance of the type from the factory.
    */
-  resolve(): T | Promise<T> {
-    return resolveProvider(this._provider);
+  resolve(registry: Registry): T | Promise<T> {
+    return resolveFactory(registry, this._factory);
   }
 
-  private _provider: Provider<T>;
+  private _factory: IFactory<T>;
 }
 
 
@@ -353,28 +305,28 @@ class SingletonResolver<T> implements IResolver<T> {
   /**
    * Construct a new transient resolver.
    */
-  constructor(provider: Provider<T>) {
-    this._provider = provider;
+  constructor(factory: IFactory<T>) {
+    this._factory = factory;
   }
 
   /**
-   * The provider managed by the resolver.
+   * The factory managed by the resolver.
    */
-  get provider(): Provider<T> {
-    return this._provider;
+  get factory(): IFactory<T> {
+    return this._factory;
   }
 
   /**
-   * Resolve an instance of the type from the provider.
+   * Resolve an instance of the type from the factory.
    */
-  resolve(): T | Promise<T> {
+  resolve(registry: Registry): T | Promise<T> {
     if (this._resolved) {
       return this._value;
     }
     if (this._promise) {
       return this._promise;
     }
-    let promise = resolveProvider(this._provider).then(value => {
+    this._promise = resolveFactory(registry, this._factory).then(value => {
       this._value = value;
       this._promise = null;
       this._resolved = true;
@@ -383,11 +335,11 @@ class SingletonResolver<T> implements IResolver<T> {
       this._promise = null;
       throw error;
     });
-    return this._promise = promise;
+    return this._promise;
   }
 
   private _value: T = null;
   private _resolved = false;
-  private _provider: Provider<T>;
+  private _factory: IFactory<T>;
   private _promise: Promise<T> = null;
 }
